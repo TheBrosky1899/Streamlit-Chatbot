33fff18e619b7c0ecd65e2809dae4ec3e0a097ac
Alex Breault <alex.breault+koch@kochcc.com>
2025-03-08 20:25:01 -0600

    working

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+
+# User authentication (dummy credentials for demo)
+USER_CREDENTIALS = {
+    "admin": {"password": "admin", "role": "admin"},
+    "user": {"password": "user", "role": "user"},
+}
+
+st.set_page_config(layout="wide")
+
+
+def authenticate(email, password) -> str:
+    """Validate user credentials and return role if authenticated."""
+    user = USER_CREDENTIALS.get(email)
+    if user and user["password"] == password:
+        return user["role"]
+    return None
+
+
+def logout_func():
+    """Clear user session."""
+    st.session_state.clear()
+    st.rerun()
+
+def login_func():
+    st.title("Login Page")
+
+    with st.form("Login", border=False):
+
+        email = st.text_input("Email", placeholder="Enter your email")
+        password = st.text_input(
+            "Password", placeholder="Enter your password"
+        )
+
+        if st.form_submit_button("Login"):
+            if role := authenticate(email, password):
+                st.session_state.authenticated = True
+                st.session_state.email = email
+                st.session_state.role = role
+                st.rerun()
+            else:
+                st.error("Invalid email or password")
+
+
+def main():
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+
+    app_pages = []
+
+    if not st.session_state.authenticated:
+        app_pages = [st.Page(login_func)]
+
+    if st.session_state.get('role', None):
+        app_pages.append(st.Page("app_pages/Home.py"))
+        app_pages.append(st.Page("app_pages/Chat.py"))
+
+    if st.session_state.get('role', None) == "admin":
+        app_pages.append(st.Page("app_pages/Upload_Training_Data.py"))
+
+    if st.session_state.authenticated:
+        app_pages.append(st.Page(logout_func, title="Logout"))
+    page_nav = st.navigation(app_pages)
+    page_nav.run()
+
+    st.rerun()
+
+
+if __name__ == "__main__":
+    main()
---
+++ app_pages/Chat.py
@@ -0,0 +1,50 @@
+from openai import OpenAI
+import os
+import streamlit as st
+
+def main():
+
+    st.title("Chatbot Wrapper")
+
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    with st.sidebar:
+        training_models = os.listdir('training_models')
+
+        selected_model = st.selectbox("Training data to use", options=[""] + training_models)
+
+        if selected_model:
+            file_list = os.listdir(f"training_models/{selected_model}")
+
+            st.write(f"Files in the {selected_model} dataset:")
+            st.write("\n\n".join([f for f in file_list]))
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+if __name__ == "__main__" or __name__ == "__page__":
+    main()
