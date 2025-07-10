1fb9753a5c4c97ecedd9a8102b29b95883feedad
TheBrosky1899 <136100204+TheBrosky1899@users.noreply.github.com>
2025-07-09 20:34:23 -0500

    init infra

--- cdk_lib/chatbot_stack.py
+++ cdk_lib/chatbot_stack.py
@@ -4,11 +4,11 @@ from aws_cdk import Stack, RemovalPolicy
 from aws_cdk.aws_s3 import Bucket
 from aws_cdk.aws_dynamodb import Table, Attribute, AttributeType, BillingMode
 from aws_cdk.aws_ec2 import Vpc
-from aws_cdk.aws_ecs import FargateTaskDefinition, ContainerImage, PortMapping, Protocol
-from aws_cdk.aws_ecs_patterns import ApplicationLoadBalancedFargateService
-from aws_cdk.aws_ecr_assets import Platform
-from aws_cdk.aws_certificatemanager import Certificate
-from aws_cdk.aws_elasticloadbalancingv2 import SslPolicy
+from aws_cdk.aws_ecs import Cluster, ContainerImage
+from aws_cdk.aws_ecs_patterns import (
+    ApplicationLoadBalancedFargateService,
+    ApplicationLoadBalancedTaskImageOptions,
+)
 
 
 class ChatbotStack(Stack):
@@ -16,6 +16,7 @@ class ChatbotStack(Stack):
 
     def __init__(self, scope, id, customer: str, **kwargs):
         super().__init__(scope, id, **kwargs)
+        self.customer = customer
         print(f"Creating chatbot stack for {customer}")
         # Create resources here
 
@@ -37,56 +38,30 @@ class ChatbotStack(Stack):
             billing_mode=BillingMode.PAY_PER_REQUEST,
         )
 
-        # Create a VPC
-        vpc = Vpc(
+        # VPC with default settings (1 NAT Gateway)
+        vpc = Vpc(self, self.build_name("StreamlitVpc"), max_azs=2)
+
+        # ECS Cluster
+        cluster = Cluster(self, self.build_name("StreamlitCluster"), vpc=vpc)
+
+        # Fargate service with load balancer using local Dockerfile
+        service = ApplicationLoadBalancedFargateService(
             self,
-            self.build_name("vpc"),
-            max_azs=2,
-        )
-
-        # Create a Fargate task definition
-        task_definition = FargateTaskDefinition(
-            self,
-            self.build_name("task-definition"),
-            cpu=256,  # TODO: optimize?
-            memory_limit_mib=512,
-        )
-
-        image = ContainerImage.from_asset(
-            directory="",
-            platform=Platform.LINUX_AMD64,
-            build_args={"STAGE": "Nah"},  # TODO: stage?
-        )
-
-        container = task_definition.add_container(
-            self.build_name("container"),
-            image=image,
-            interactive=True,
-            pseudo_terminal=True,
-            secrets={},
-            environment={},
-        )
-
-        container.add_port_mappings(
-            PortMapping(container_port=6969, protocol=Protocol.TCP)
-        )
-
-        # Create a certificate
-        certificate = Certificate(
-            self,
-            self.build_name("certificate"),
-            domain_name=f"{self.customer}.example.com",  # TODO: bring your own domain?
-        )
-
-        fargate_service = ApplicationLoadBalancedFargateService(
-            self,
-            self.build_name("fargate-service"),
+            self.build_name("StreamlitFargateService"),
+            cluster=cluster,
+            cpu=512,
+            memory_limit_mib=1024,
+            desired_count=1,
             public_load_balancer=True,
-            task_definition=task_definition,
-            redirect_http=True,
-            certificate=certificate,
-            ssl_policy=SslPolicy.RECOMMENDED,
-            vpc=vpc,
+            task_image_options=ApplicationLoadBalancedTaskImageOptions(
+                image=ContainerImage.from_asset("."),
+                container_port=6969,
+                environment={
+                    "BUCKET_NAME": bucket.bucket_name,
+                    "USER_TABLE_NAME": user_management_table.table_name,
+                    "CUSTOMER": self.customer,
+                },
+            ),
         )
 
     def build_name(self, name: str) -> str:
